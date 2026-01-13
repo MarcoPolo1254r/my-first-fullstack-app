722fb32ba10c260458856978e6e204c29914d321
lucas <marcopolo1254rn@gmail.com>
2026-01-13 15:19:22 -0300

    adding users routes+controlers in the application

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const notesRoutes = require('./routes/notesRoutes');
+const usersRoutes = require('./routes/usersRoutesRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use('/notes', notesRoutes);
+app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => {
   res.send('API running');
